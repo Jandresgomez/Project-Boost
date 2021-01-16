492ab1d260762730a7b711c4d5bc4d091b2675aa
Jorge Gomez <jagv1998@gmail.com>
2021-01-16 16:18:28 -0500

    Added tags and reactions to non friendly entities

--- Assets/Scenes/Rocket/Rocket.cs
+++ Assets/Scenes/Rocket/Rocket.cs
@@ -6,12 +6,20 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource shipThruster;
+    [SerializeField] float mainThrust = 120f;
+    [SerializeField] float rcsThrust = 120f;
+
+    // Previous position
+    Vector3 originalPosition;
+    Quaternion originalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         shipThruster = GetComponent<AudioSource>();
+        this.originalPosition = this.transform.position;
+        this.originalRotation = this.transform.rotation;
     }
 
     // Update is called once per frame
@@ -22,6 +30,26 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do something
+                break;
+            default:
+                transform.SetPositionAndRotation(originalPosition, originalRotation);
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        rigidBody.freezeRotation = true; // Avoid sticking forces to the ship
 
         if (Input.GetKey(KeyCode.Space))
         {
@@ -29,29 +57,28 @@ public class Rocket : MonoBehaviour
             {
                 shipThruster.Play();
             }
-            rigidBody.AddRelativeForce(Vector3.up);
+            float thrustSpeedForFrame = Time.deltaTime * mainThrust;
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeedForFrame);
         }
         else
         {
             shipThruster.Stop();
         }
 
+        rigidBody.freezeRotation = false; // Avoid sticking forces to the ship
+    }
+
+    private void Rotate()
+    {
+        float rotationSpeedForFrame = Time.deltaTime * rcsThrust;
+
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeedForFrame);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(-Vector3.left);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(Vector3.left);
+            transform.Rotate(Vector3.forward * rotationSpeedForFrame);
         }
     }
 }
